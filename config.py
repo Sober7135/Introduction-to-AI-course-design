fb1fd427ce1b31bf509fea1ab30e2d7542f38f49
Sober7135 <soberwu@outlook.com>
2023-11-24 21:43:13 +0800

    fix: wrong split ratio.

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 epochs = 500
+split_ratio = 0.8
--- gen.py
+++ gen.py
@@ -1,5 +1,6 @@
 import torch
 from sklearn.datasets import make_circles
+from config import *
 
 # Generate the dataset with two concentric circles
 coordinates, labels = make_circles(n_samples=500, noise=0.075, factor=0.5, random_state=2023)
@@ -9,7 +10,7 @@ coordinates = torch.tensor(coordinates, dtype=torch.float)
 labels = torch.tensor(labels.reshape(-1, 1), dtype=torch.float)
 
 # Split the dataset into training and testing sets
-split = int(0.99 * len(coordinates))
+split = int(split_ratio * len(coordinates))
 coordinates_train, labels_train = coordinates[:split], labels[:split]
 coordinates_test, labels_test = coordinates[split:], labels[split:]
 
